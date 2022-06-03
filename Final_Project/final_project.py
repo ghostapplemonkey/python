30763ac59ae5cae2e039194ef97b3fb81e8ddd68
Yong <ghostapplemonkey@gmail.com>
2022-06-03 14:25:59 +0800

    final project change

--- Final_Project/final_project.py
+++ Final_Project/final_project.py
@@ -1,3 +1,4 @@
+from turtle import color
 from pycat.core import Window, Sprite, Color, KeyCode, RotationMode, Scheduler
 
 window = Window(width=1200,height=600)
@@ -49,12 +50,51 @@ class Knife(Sprite):
         else:
             self.is_following = True
                 
+class Ax(Sprite):
+    def on_create(self):
+        self.image = "img/sword.png"
+        self.scale = 0.3
+        self.is_following = True
+    def on_update(self, dt):
+        if self.is_following == True:
+            if self.scale_x < 0:
+                self.x = p2.x + 30
+                self.y = p2.y + 38
+                self.rotation = 0
+            else:
+                self.x = p2.x - 30
+                self.y = p2.y + 38
+                self.rotation = 0
+        else:
+            if self.scale_x < 0:
+                self.x = p2.x + 66
+                self.y = p2.y
+                self.rotation = -60
+            else:
+                self.x = p2.x - 66
+                self.y = p2.y 
+                self.rotation = 60
+        if window.is_key_down(KeyCode.M):
+            if self.scale_x < 0:
+                self.is_following = False
+                Scheduler.wait(0.15,self.knife_back)
+            else:
+                self.is_following = False
+                Scheduler.wait(0.15,self.knife_back)
+        
+    def knife_back(self):
+        if self.scale_x < 0:
+            self.is_following = True
+        else:
+            self.is_following = True
+                
 
         
 
 class P1(Sprite):
     def on_create(self):
         self.x = 325
+        self.color = Color.BLUE
         self.y = 375
         self.image = "img/stop.png"
         self.ysp = 0
@@ -121,9 +161,81 @@ class P1(Sprite):
             self.is_jump = False
             self.ysp = 0
 
+
+class P2(Sprite):
+    def on_create(self):
+        self.x = 825
+        self.y = 375
+        self.image = "img/stop.png"
+        self.color = Color.RED
+        self.ysp = 0
+        self.is_jump = False
+        self.layer = 2
+        self.state = 0
+        self.time = 0
+        self.scale = 0.4
+    def on_update(self, dt):
+        self.time += dt
+        if window.is_key_pressed(KeyCode.RIGHT):
+            self.x += 7
+            self.state = 1
+        elif window.is_key_pressed(KeyCode.LEFT):
+            self.x -= 7
+            self.state = -1
+        else:
+            self.state = 0
+        if self.state == 0:
+            self.time = 0
+            self.image = "img/stop.png"
+        if self.state == 1:
+            if self.time <= 0.1:
+                self.image = "img/move1.png"
+            if self.time > 0.1 and dt <= 0.2:
+                self.image = "img/move2.png"
+            if self.time > 0.2 and dt <= 0.3:
+                self.image = "img/move3.png"
+            if self.time > 0.3 and dt <= 0.4:
+                self.image = "img/move4.png"
+            if self.time > 0.4 and dt <= 0.5:
+                self.image = "img/move5.png"
+            if self.time > 0.5:
+                self.time = 0
+            if ax.scale_x > 0:
+                ax.scale_x *= -1
+                
+            
+        if self.state == -1:
+            if self.time <= 0.1:
+                self.image = "img/move1 (2).png"
+            if self.time > 0.1 and dt <= 0.2:
+                self.image = "img/move2 (2).png"
+            if self.time > 0.2 and dt <= 0.3:
+                self.image = "img/move3 (2).png"
+            if self.time > 0.3 and dt <= 0.4:
+                self.image = "img/move4 (2).png"
+            if self.time > 0.4 and dt <= 0.5:
+                self.image = "img/move5 (2).png"
+            if self.time > 0.5:
+                self.time = 0
+            if ax.scale_x < 0:
+                ax.scale_x *= -1
+                
+        if window.is_key_pressed(KeyCode.UP) and self.is_jump == False:
+            self.ysp = 18
+            self.is_jump = True
+        self.y += self.ysp
+        self.ysp -= 0.8
+        ground = self.get_touching_sprites_with_tag("ground")
+        if ground and self.ysp < 0:
+            groundt = ground[0]
+            self.y = groundt.y + groundt.height/2 + self.height/2
+            self.is_jump = False
+            self.ysp = 0
         
 
         
 p1 = window.create_sprite(P1)
+p2 = window.create_sprite(P2)
 knife = window.create_sprite(Knife)
+ax = window.create_sprite(Ax)
 window.run()
